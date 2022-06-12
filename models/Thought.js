a3f8225f2c87ebac40eedd82ba82b9db9d40ee04
Colleen Murray <colleenkhm@gmail.com>
2022-06-11 19:56:25 -0500

    initialized models

--- models/Thought.js
+++ models/Thought.js
@@ -1,1 +1,26 @@
 const { Schema, model } = require('mongoose');
+const Thought = model('Thought', ThoughtSchema);
+
+const ThoughtSchema = new Schema ({
+    thoughtText: {
+        type: String
+        // required
+        // between 1 and 280 characters
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+        // getter method to format timestamp on query
+    },
+    username: {
+        type: String
+        // required
+    },
+    reactions: {
+        // array of nested documents created with reaction schema
+    }
+
+    //Schema settings - create virtual called reactionCount that retrieves length of thought's reactions array field on query
+})
+
+module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -1,1 +1,29 @@
 const { Schema, model } = require('mongoose');
+const User = model('User', UserSchema);
+
+const UserSchema = new Schema ({
+    username: {
+        type: String,
+        //unique
+        //required
+        //trimmed
+    },
+    email: {
+        type: String,
+        //required
+        //unique
+        //must match valid email address (google mongoose matching validation)
+    },
+    thoughts: {
+        // id: []
+        // array of id values referencing thought model
+    },
+    friends: {
+        // array of id values referencing user model (self-reference)
+    }
+
+    //Schema Settings
+    // create virtual called friendCount that retrieves length of user's friends array field on query
+});
+
+module.exports = User;
