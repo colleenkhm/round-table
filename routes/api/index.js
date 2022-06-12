39ac49db241d7425eea15e90093fe4101878d3d5
Colleen Murray <colleenkhm@gmail.com>
2022-06-12 16:57:22 -0500

    index.js for routes and api

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
-const {thoughtRoutes, userRoutes} = require('./routes');
 
-router.use('')
+const userRoutes = require('./user-routes');
+const thoughtRoutes = require('./thought-routes');
+
+router.use('/users', userRoutes);
+router.use('/thoughts', thoughtRoutes);
+
+module.exports = router;
